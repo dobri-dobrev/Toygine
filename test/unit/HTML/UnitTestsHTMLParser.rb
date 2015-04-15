60a0f261b08af79fa4772ad7e63388a5758d357e
dobri <dobri@yahoo-inc.com>
2015-04-15 07:36:02 -0700

    more htmlParser tests and minor bug fixes with the parser

--- test/unit/HTML/UnitTestsHTMLParser.rb
+++ test/unit/HTML/UnitTestsHTMLParser.rb
@@ -8,6 +8,61 @@ class UnitTestsHTMLParser <Test::Unit::TestCase
     fr = FileReader.new(mock_file, "test_path")
     cp = HTMLParser.new(fr)
   end
-  
+	
+	def test_is_at_beginning_of_element
+	  mock_file = MockFile.new(["<Hello"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)	
+	  assert_equal(true, cp.is_at_beginning_of_element())
+	  mock_file = MockFile.new(["</Hello"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)	
+	  assert_equal(false, cp.is_at_beginning_of_element())
+	end
+
+	def test_is_at_closing_of_element
+	  mock_file = MockFile.new(["<Hello"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)	
+	  assert_equal(false, cp.is_at_closing_of_element())
+	  mock_file = MockFile.new(["</Hello"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)	
+	  assert_equal(true, cp.is_at_closing_of_element())
+	end  
+
+	def test_consume_closing_tag
+	  mock_file = MockFile.new(["<Hello  !>b"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)	
+	  cp.consume_closing_tag()
+	  assert_equal('b', fr.current_char())
+	end
+
+	def test_consume_attribute_value
+	  mock_file = MockFile.new(["'biggie'z"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)	
+	  assert_equal('biggie', cp.consume_attribute_value())
+	  assert_equal('z', fr.current_char())
+	end
+
+	def test_attribute_pair
+		mock_file = MockFile.new(["small = 'big'"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)
+	  pair = cp.consume_attribute_pair()	
+	  assert_equal('small', pair[0])
+	  assert_equal('big', pair[1])
+	end
+
+	def test_consume_attribute
+		mock_file = MockFile.new(["small = 'big' second='first' >"])
+	  fr = FileReader.new(mock_file, "test_path")
+	  cp = HTMLParser.new(fr)
+	  attrs = cp.consume_attributes()
+	  assert_equal('big', attrs["small"])
+	  assert_equal('first', attrs["second"])
+	end
 end
 
