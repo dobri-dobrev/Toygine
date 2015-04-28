e2c8bc1f02164e1400b5ccac75d73315d677baae
dobri-dobrev <dmd2169@columbia.edu>
2015-04-27 21:08:59 -0700

    adding style node to layout box

--- lib/node/LayoutBox.rb
+++ lib/node/LayoutBox.rb
@@ -1,9 +1,10 @@
 class LayoutBox < BaseNode
-  attr_accessor :dimensions, :box_type, :children
+  attr_accessor :dimensions, :box_type, :style_node, :children
 
-  def initialize(dim, type, chil)
+  def initialize(dim, type, s_n, chil)
     @dimensions = dim
     @box_type = type
+    @style_node = s_n
     @children = chil
   end
 end
--- test/unit/layout/UnitTestsLayoutBox.rb
+++ test/unit/layout/UnitTestsLayoutBox.rb
@@ -3,14 +3,15 @@ require_relative '../../../lib/Toygine.rb'
 
 class UnitTestsLayoutBox <Test::Unit::TestCase
   def test_initialize
-    t = LayoutBox.new(1, 2, 3)
+    t = LayoutBox.new(1, 2, 3, 4)
     assert_equal(1, t.dimensions)
     assert_equal(2, t.box_type)
-    assert_equal(3, t.children)
+    assert_equal(3, t.style_node)
+    assert_equal(4, t.children)
   end
 
   def test_add_child
-    t = LayoutBox.new(1, 2, [])
+    t = LayoutBox.new(1, 2, 3, [])
     assert_equal(0, t.children.length)
     t.add_child(2)
     assert_equal(1, t.children.length)
